3b7948e12cb43a4f4618f1ee74b254f2f06ee5cb
Raphael <pusam.raphy@gmail.com>
2023-12-07 20:58:48 +0100

    Changes: - checkt ob packages noch vorhanden sind - zieht vom user 5 münzen ab, wenn möglich - entfernt package aus table nach kauf - paar debug messages warum es (richtig) failed

--- src/application/persistance/repository/PackageRepository.java
+++ src/application/persistance/repository/PackageRepository.java
@@ -68,11 +68,135 @@ public class PackageRepository implements PackageRepositoryInterface {
         }
     }
 
+    //////////////////////////////////////////////////////////////////////////////
+
+    private boolean adjustUserCoins(String token){
+        int amount = 0;
+
+        // 1. check if user has enough coins
+        try(PreparedStatement preparedStatement = this.unitOfWork.prepareStatement(
+                """
+                SELECT (coins) FROM public.users WHERE authtoken = ?
+                """
+        )){
+            preparedStatement.setString(1, token);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            // return false if not
+            if (resultSet.next()){
+                amount = resultSet.getInt(1);
+            }
+
+            if(amount < 5) {
+                System.out.println("not enough coins from " + token);
+                return false;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Couldn't query for coins", e);
+        }
+
+        // 2. if enough coins -> co(i)ntinue and update user table
+        try (PreparedStatement preparedStatement = this.unitOfWork.prepareStatement(
+                """
+                UPDATE public.users SET coins = ? WHERE authtoken = ?
+                """
+        )) {
+
+            preparedStatement.setInt(1, amount - 5);
+            preparedStatement.setString(2, token);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected <= 0) {
+                // Handle the case where the update fails for one or more UUIDs
+                return false;
+            }
+
+            this.unitOfWork.commitTransaction();
+            System.out.println("Coins updated for user " + token);
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Updating coins in user failed");
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////
+
+    private boolean checkIfPackagesAvailable(){
+        int entryCnt = 0;
+
+        try(PreparedStatement preparedStatement = this.unitOfWork.prepareStatement(
+                """
+                SELECT COUNT(*) AS entry_count FROM public.packages
+                """
+        )){
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()){
+                entryCnt = resultSet.getInt("entry_count");
+            } else{
+                return false;
+            }
+
+            if(entryCnt < 1) {
+                System.out.println("no packages left");
+                return false;
+            }
+
+            return true;
+        } catch (SQLException e) {
+            throw new DataAccessException("Couldn't query for package count", e);
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////////////////
+
+    private boolean removePackage(int packageID){
+        try (PreparedStatement preparedStatement = this.unitOfWork.prepareStatement(
+                """
+                DELETE FROM public.packages WHERE packageid = ?
+                """
+        )) {
+
+            preparedStatement.setInt(1, packageID);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected <= 0) {
+                // Handle the case where the delete fails
+                return false;
+            }
+
+            this.unitOfWork.commitTransaction();
+            System.out.println("Package deleted after consumption");
+            return true;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////////////////
+
     @Override
     public boolean acquirePackage(String token) {
         UUID[] cardUUIDs = new UUID[5];
         Card[] cards = new Card[5];
-        int packageId;
+        int packageId = 0;
+
+        //0. check if user has enough coins and if yes, subtract 5
+        //0. check if enough packages available
+
+        if(!checkIfPackagesAvailable()){
+            return false;
+        }
+
+        if(!adjustUserCoins(token)){
+            return false;
+        }
 
         //1. pick random package from Package-DB and extract card-UUIDs
         try (PreparedStatement preparedStatement = this.unitOfWork.prepareStatement(
@@ -84,14 +208,14 @@ public class PackageRepository implements PackageRepositoryInterface {
             ResultSet resultSet = preparedStatement.executeQuery();
 
             if (resultSet.next()) {
+                //set class variables
                 packageId = resultSet.getInt(6);
-
-                //ins
                 for(int i = 0; i<5; i++){
                     cardUUIDs[i] = (UUID) resultSet.getObject(i+1);
                 }
             }
         } catch (SQLException e) {
+            System.out.println("Selecting random package with cards from DB failed");
             throw new RuntimeException(e);
         }
 
@@ -109,15 +233,18 @@ public class PackageRepository implements PackageRepositoryInterface {
 
                 if (rowsAffected <= 0) {
                     // Handle the case where the update fails for one or more UUIDs
+                    System.out.println("Adding cards to user stack failed");
                     return false;
                 }
             }
 
             this.unitOfWork.commitTransaction();
             System.out.println("Cards added to stack");
-            return true;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
+        //3. remove package from DB
+        return removePackage(packageId);
     }
 }
